39911d60b6033f46ccb35f23b060dd7319f5d87c
Hakob1an <hakob1an733@gmail.com>
2024-09-21 23:23:53 +0400

    smth

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     MainWindow w;
-    qDebug() << "Available drivers:" << QSqlDatabase::drivers();
     w.show();
     return a.exec();
 }
